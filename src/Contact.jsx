63066d3f7aa9312406e1c528072cf6e3b19cf916
sharmaketan03 <ketan301024@gmail.com>
2025-06-02 13:58:58 +0530

    on working

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { GrFormCalendar } from "react-icons/gr"
 
 // let array=[]
 function Contact() {
@@ -10,7 +11,7 @@ function Contact() {
 
     })
 
-   const [save,setSavedData] =useState({})
+   const [save,setSavedData] =useState([])
 
     function handel(e){
         
@@ -22,28 +23,62 @@ function Contact() {
         }
     function handleSubmit(e){
             e.preventDefault()
-            setSavedData({...formdata})
-            // console.log(save)
+            
+           setSavedData([...save,formdata])
+          
     }
+    useEffect(()=>{
+        if(save.length>0){
+          localStorage.setItem("save",JSON.stringify(save))
+          setformdata({
+             name:"",
+         email:"",
+         Phonenumber:"",
+         textarea:""
+
+          },[save])
+        }
+    },[])
+   
 
   return (
     <>
-     <form action="" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name</label>
+    <div className="map">
+    <div className="setmap">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3559.241553841478!2d75.78974481125746!3d26.864065576578838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db3f27d3dad07%3A0xb2641415d32e0c18!2sFull%20Stack%20Learning!5e0!3m2!1sen!2sin!4v1748846291965!5m2!1sen!2sin" width="100%" height="450" style={{border:"2px solid black" , borderRadius:"8px"}}   loading="lazy" ></iframe>
+    </div>
+    <div className="form">
+     <div>
+ <form action="" onSubmit={handleSubmit}>
+<label htmlFor="name">Name</label>
+   <div>
           <input type="text" name="name" id="name"  placeholder="Enter Your Name" value={formdata.name} onChange={handel}/>
-          <label htmlFor="email">Email</label>
+   </div>
+     <label htmlFor="email">Email</label>
+   <div>
           <input type="email" name="email" id="email" placeholder="Enter Your E-mail"  value={formdata.email} onChange={handel}/>
-          <label htmlFor="phonenumber">Phone Number</label>
+   </div>
+ <label htmlFor="phonenumber">Phone Number</label>
+      <div>
           <input type="text" name="Phonenumber" id="phonenumber" placeholder="Enter Your Number"  value={formdata.Phonenumber} onChange={handel}/>
-          <textarea name="textarea" id="" vlaue={formdata.textarea} onChange={handel} placeholder="Enter Your Text Message"></textarea>
-          <button  type="sumbit">sumbit</button>
+        </div>    
+         <div>
+ <textarea name="textarea" id="" vlaue=
+          {formdata.textarea} onChange={handel} placeholder="Enter Your Text Message" rows={5} cols={5}></textarea>
+         </div>
+         
+         
+          <button  type="sumbit">sumbit</button> 
      </form>
-     <ul>
-        <li>{save.name}</li>
-        <li>{save.email}</li>
-        <li>{save.Phonenumber}</li>
-        <li>{save.textarea}</li>
-     </ul>
+     </div>
+       
+    
+
+     
+    </div>
+    </div>
+    
+     
     </>
   )
 }
--- src/First.jsx
+++ src/First.jsx
@@ -7,6 +7,7 @@ function First() {
     <>
      <Header/>
      <Outlet/>
+     <footer/>
     </>
   )
 }
