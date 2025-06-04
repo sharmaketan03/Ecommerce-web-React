424ef149a920ffad8f1c068ef5be6225b27abc12
sharmaketan03 <ketan301024@gmail.com>
2025-06-04 11:07:00 +0530

    error solving pratice

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom"
-import { UserContext } from "./src/Singleproject.jsx"
+import { UserContext } from "./Singleproject"
 
 
 function Header() {
